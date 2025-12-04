eea4282180edd13ba38c574f32ca43ca560a7890
Manju760-arch <manjusivaa6634@gmail.com>
2025-12-04 19:26:19 +0530

    Commit

--- client/src/components/ProductForm.js
+++ client/src/components/ProductForm.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+
+import React, { useState, useEffect } from "react";
 
 export default function ProductForm({ onSubmit, initial = null }) {
   useEffect(() => {
