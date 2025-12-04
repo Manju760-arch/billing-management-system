2fba4a17455d46aa8485acd88c42dbd67333c68e
Manju760-arch <manjusivaa6634@gmail.com>
2025-12-04 20:24:28 +0530

    Commit

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL:'https://billing-management-system-2.onrender.com/api',
   timeout: 10000,
 });
 
